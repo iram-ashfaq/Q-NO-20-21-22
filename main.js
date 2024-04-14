991d7884c09fc0f723ec54a2d62e5d0589f63f0e
Iram.Ashfaq <161501174+iram-ashfaq@users.noreply.github.com>
2024-04-14 23:19:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// question number 20
+var country = ["Pakistan", "paris", "newyork", "chaina", "japan"];
+console.log("list of country:");
+console.log(country);
+// question number 21:
+var person = { name: "iram", fname: "female", age: 20, };
+console.log(person);
+// question number 22
+var days = ["sunday", "monday", "tuesday", "wedneday", "thurday", "friday", "saturday", "sunday"];
+//console.log(days{7})
+console.log(days[6]);
---
+++ main.ts
@@ -0,0 +1,14 @@
+// question number 20
+let country: string[] = ["Pakistan","paris","newyork","chaina","japan" ]
+console.log("list of country:")
+console.log(country)
+
+// question number 21:
+let person: {name: string,fname: string , age: number} = {name:"iram", fname: "female", age: 20 ,}
+console.log(person)
+
+// question number 22
+const days : string [] = ["sunday", "monday", "tuesday", "wedneday","thurday","friday","saturday","sunday"]
+//console.log(days{7})
+console.log(days[6])
+
